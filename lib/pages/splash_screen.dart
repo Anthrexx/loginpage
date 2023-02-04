68890f0f7990d030f454a5066527943095f87f8e
visahlmech <vishalchoudhary5225@gmail.com>
2023-02-04 17:16:21 +0530

    third commmit

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return  const Scaffold(
       body: Center(
-        child: Text("firebase",style: TextStyle(fontSize: 30),),
+        child: Text("NIMBUS",style: TextStyle(fontSize: 30),),
       ),
     );
   }
